6444b45f336af7ca450d13a3516b696fc8e0ea14
Unknown <muhajir16@mhs.if.its.ac.id>
2017-11-28 11:30:45 +0700

    Add command touch.c

---
+++ touch.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+#define O_CREAT 0x0200 // create if nonexistant
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf(2, "Help ...\n");
+	} else {
+		int i;
+		for (i = 1; i < argc; i++) {
+			open(argv[i], O_CREAT);
+		}
+	}
+
+	exit();
+}
